ea4167535f8c749a61869b303a71199f1ad28eac
Sebastián Calderon <sebastiancalderoncarrillo@gmail.com>
2026-02-27 22:49:58 -0600

    Working version. Tests to be done

--- problema_2/problema_2.c
+++ problema_2/problema_2.c
@@ -15,20 +15,18 @@ typedef struct nodo_lista
     struct nodo_lista *siguiente;
 } NODO_LISTA;
 
-char obtener_cadena();
+char *obtener_palabra();
 int obtener_opcion();
 
 int verificar_palindromo();
 
-void lista_agregar_final(NODO_LISTA *, char caracter);
+void lista_agregar_final(NODO_LISTA **, char caracter);
 char lista_pop_primero(NODO_LISTA **);
 void limpiar_lista(NODO_LISTA **);
-void imprimir_lista(NODO_LISTA *);
 
 void pila_agregar(NODO_PILA **, char caracter);
 char pila_pop(NODO_PILA **);
 void limpiar_pila(NODO_PILA **);
-void imprimir_pila(NODO_PILA *);
 
 int main()
 {
@@ -63,30 +61,68 @@ int main()
     return 0;
 }
 
-// POR IMPLEMENTAR
-// CAMBIAR DE LECTURA DE CARACTER A LECTURA DE CADENAS
-char obtener_cadena()
+// Validacion para obtener una cadena que solo contenga una palabra con letras del abecedario
+char *obtener_palabra()
 {
-    char Aux[' '];
-    do
+    char temp[256];
+    int valida = 0;
+
+    while (!valida)
     {
-        printf("\nIntroduzca un caracter: ");
+        valida = 1;
         fflush(stdin);
-        scanf(" %s", Aux); // se leen los datos introducidos
+        getchar(); // obtener el '\n' que hubo anteriormente
+        printf("Ingrese una palabra: ");
         fflush(stdin);
 
-        if (strlen(Aux) == 1 && isalpha(Aux[0]))
+        // Obtener TODO lo que haya en stdin
+        fgets(temp, sizeof(temp), stdin);
+
+        // Reemplazar '/n' por un '\0'
+        temp[strcspn(temp, "\n")] = '\0';
+
+        // Revisar espacios
+        char *espacio = strchr(temp, ' ');
+        if (espacio != NULL)
         {
-            return Aux[0];
+            printf("ERROR. Se ingreso mas de una palabra. Oprima cualquier tecla");
+            valida = 0;
         }
-        else
+
+        // Revisar cadena vacia
+        if (strlen(temp) == 0)
         {
-            printf("\n\n Error, dato mal introducido\n\n ");
-            system("pause");
+            printf("ERROR. No se ingreso ninguna palabra. Oprima cualquier tecla");
+            valida = 0;
         }
-    } while (1);
+
+        // Validar que todas sean letras del abecedario
+        // o que sean espacios (aunque no sean validos, el error se atrapa en condicional anterior)
+        int i;
+        for (i = 0; temp[i] != '\0'; i++)
+        {
+            if (!isalpha(temp[i]) && temp[i] != ' ')
+            {
+                valida = 0;
+                printf("ERROR. La palabra no contiene solo letras. Oprima cualquier tecla");
+                break;
+            }
+        }
+    }
+    // Reservar memoria exacta ahora que temp esta limpio
+    // y podemos saber su tamaño exacto
+    char *palabra = malloc(strlen(temp) + 1); // Se agrega uno para incluir el '\0'
+
+    // Revisar alocacion de memoria
+    if (palabra == NULL)
+        return NULL;
+
+    // Pasar palabra de variable estatica a string dinamica
+    strcpy(palabra, temp);
+    return palabra;
 }
 
+// Funcion para obtener numeros enteros positivos + 0
 int obtener_opcion()
 {
     char Aux[' '];
@@ -96,7 +132,6 @@ int obtener_opcion()
         printf("\nIntroduzca una opcion: ");
         fflush(stdin);
         scanf(" %s", Aux); // se lee los datos introducidos
-        fflush(stdin);
         y = strlen(Aux);
 
         for (i = 0; i < y; i++)
@@ -120,24 +155,112 @@ int obtener_opcion()
     return num;
 }
 
+// Funcion principal para verificar si una palabra es palindromo
 int verificar_palindromo()
 {
     NODO_LISTA *cabecera_lista = NULL;
     NODO_PILA *cabecera_pila = NULL;
+    char *cadena = obtener_palabra();
+    int es_palindromo = 1, i = 0;
+
+    // printf("cadena: %s\nlargo:%d\nprimero: %c\nultimo:%c\n", cadena, strlen(cadena), cadena[0], cadena[strlen(cadena) - 1]);
+
+    // agregar cadena a pila y a lista.
+    // En la pila se agrega al reves, de derecha a izquierda
+    // En la lista se agrega en orden, de izquiera a derecha
+    while (cadena[i] != '\0')
+    {
+        pila_agregar(&cabecera_pila, cadena[i]);
+        lista_agregar_final(&cabecera_lista, cadena[i]);
+        i++;
+    }
+
+    // Hace pop a lista y a pila simultaneamente, compara los caracteres y cambia la bandera si no son iguales
+    for (i = 0; i < strlen(cadena); i++)
+    {
+        if (pila_pop(&cabecera_pila) != lista_pop_primero(&cabecera_lista))
+        {
+            es_palindromo = 0;
+            break;
+        }
+    }
 
     limpiar_lista(&cabecera_lista);
     limpiar_pila(&cabecera_pila);
-    return 1;
+    free(cadena);
+
+    if (es_palindromo)
+        printf("SI ES PALINDROMO\n");
+    if (!es_palindromo)
+        printf("NO ES PALINDROMO\n");
+    system("pause");
+    return es_palindromo;
 }
 
-void lista_agregar_final(NODO_LISTA *cabecera_lista, char caracter)
+// Agrega al final de una lista encadenada
+void lista_agregar_final(NODO_LISTA **cabecera_lista, char caracter)
 {
+    NODO_LISTA *nuevo = malloc(sizeof(NODO_LISTA));
+
+    // Verificar que se haya alocado memoria
+    if (nuevo == NULL)
+    {
+        printf("Error de alocacion de memoria de lista.\n");
+        system("pause");
+        return;
+    }
+    else
+    {
+        NODO_LISTA *actual, *previo;
+        previo = NULL;
+        actual = *cabecera_lista;
+        while (actual != NULL)
+        {
+            previo = actual;
+            actual = actual->siguiente;
+        }
+        // La lista no tenia nodos
+        if (previo == NULL)
+        {
+            nuevo->caracter = caracter;
+            nuevo->siguiente = NULL;
+            *cabecera_lista = nuevo;
+            return;
+            // Tiene uno o mas nodos
+        }
+        else
+        {
+            nuevo->caracter = caracter;
+            nuevo->siguiente = NULL;
+            previo->siguiente = nuevo;
+            return;
+        }
+    }
 }
 
-char lista_pop_primero(NODO_LISTA **)
+// Pop al inicio de una lista encadenada
+char lista_pop_primero(NODO_LISTA **cabecera_lista)
 {
+    // la lista esta vacia
+    if (*cabecera_lista == NULL)
+    {
+        printf("ERROR: La lista esta vacia.\n");
+        system("pause");
+        return '\0';
+    }
+    else
+    {
+        NODO_LISTA *temporal;
+        char caracter;
+        temporal = *cabecera_lista;
+        *cabecera_lista = temporal->siguiente;
+        caracter = temporal->caracter;
+        free(temporal);
+        return caracter;
+    }
 }
 
+// Recorrido y limpieza de lista encadenada
 void limpiar_lista(NODO_LISTA **cabecera_lista)
 {
     NODO_LISTA *previo, *actual;
@@ -146,9 +269,7 @@ void limpiar_lista(NODO_LISTA **cabecera_lista)
 
     // La lista esta vacia
     if (actual == NULL)
-    {
         return;
-    }
 
     // La lista contiene al menos un elemento
     else
@@ -164,16 +285,12 @@ void limpiar_lista(NODO_LISTA **cabecera_lista)
     }
 }
 
-void imprimir_lista(NODO_LISTA *)
-{
-}
-
 // Agrega al inicio de una lista encadenada
 void pila_agregar(NODO_PILA **cabecera_pila, char caracter)
 {
     NODO_PILA *nuevo = malloc(sizeof(NODO_PILA));
 
-    // Verificar que se halla alocado memoria
+    // Verificar que se haya alocado memoria
     if (nuevo == NULL)
     {
         printf("Error de alocacion de memoria de pila.\n");
@@ -211,6 +328,7 @@ char pila_pop(NODO_PILA **cabecera_pila)
     }
 }
 
+// Recorrido y limpieza de lista encadenada
 void limpiar_pila(NODO_PILA **cabecera_pila)
 {
     NODO_PILA *previo, *actual;
@@ -219,9 +337,7 @@ void limpiar_pila(NODO_PILA **cabecera_pila)
 
     // La lista esta vacia
     if (actual == NULL)
-    {
         return;
-    }
 
     // La lista contiene al menos un elemento
     else
@@ -235,8 +351,4 @@ void limpiar_pila(NODO_PILA **cabecera_pila)
         *cabecera_pila = NULL;
         return;
     }
-}
-
-void imprimir_pila(NODO_PILA *)
-{
 }
